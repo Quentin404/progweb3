3371d0c9d891d09d7a82474176bffb034ad828e2
unknown <quentinhuet4500@gmail.com>
2023-03-22 11:15:59 +0100

    album list done

---
+++ src/components/albumRow.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="albumRow">
+        <div class="albumArt">
+            <img src="https://lastfm.freetls.fastly.net/i/u/174s/a217ddc5794aecd2081bda7e2e789782.jpg" alt="">
+            <div class="textTitles">
+                <span class="textArtist">artist</span>
+                <span class="textAlbum">album_name</span>
+            </div>
+        </div>
+        <div class="albumStats">
+            <span class="textInfos" alt="nb tracks">999</span> 
+            <span class="textInfos" alt="nb plays">72120</span>
+            <span class="textInfos" alt="lenght">10:23</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'albumRow'
+    }
+</script>
+
+<style scoped>
+    .albumRow {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        background-color: #cacaca;
+        border-radius: 10px;
+    }
+    
+    .albumArt {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: center;
+        height: 3.5rem;
+        flex-grow: 1.5;
+    }
+
+    img {
+        height: 100%;
+        width: auto;
+        margin-right: 2rem;
+        border-radius: 10px;
+    }
+
+    .textTitles {
+        display: flex;
+        flex-direction: column;
+        justify-self: left;
+    }
+
+    .textArtist {
+        font-size: 14px;
+    }
+
+    .textAlbum{
+        font-size: 12px;
+    }
+
+    .albumStats  {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-grow: 1;
+    }
+
+    .textInfos {
+        font-size: 16px;
+    }
+</style>
---
+++ src/components/rowList.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="rowList">
+        <h2>Planned</h2>
+        <div class="albumList">
+            <albumRow/>
+            <albumRow/>
+            <albumRow/>
+            <albumRow/>
+            <albumRow/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import albumRow from "./albumRow"
+
+    export default {
+        name: 'rowList',
+        components: {
+            albumRow
+        }
+    }
+</script>
+
+<style scoped>
+    .rowList {
+        max-width: 75vw;
+        padding: .2rem 1rem 1rem;
+
+        background-color: #eaeaea;
+        border-radius: 10px;
+    }
+
+    .albumList > * {
+        margin-bottom: 0.5rem;
+    }
+
+    .albumList > *:last-child {
+        margin-bottom: 0px;
+    }
+</style>
