266c1b330c676a9cbddd60a7e8f81c3868a06202
Quentin4h04 <quentinhuet4500@gmail.com>
2023-04-10 17:35:24 +0200

    function chain that works for getting albums

--- src/components/rowList.vue
+++ src/components/rowList.vue
@@ -1,3 +1,4 @@
+<!-- :cover_art="album.image[1]['#text']" -->
 <template>
     <div class="rowList">
         <h2>Planned</h2>
@@ -6,8 +7,8 @@
                 v-for="album in albumData" 
                 :key="album.name"
                 :artist="album.artist"
-                :name="album.name"
                 :cover_art="album.image[1]['#text']"
+                :name="album.name"
                 :nb_tracks="album.streamable"
                 :nb_plays="album.streamable"
                 :length="album.streamable"
@@ -18,6 +19,7 @@
 
 <script>
     import albumRow from "./albumRow"
+    import {getAlbumData} from '@/services/api/lastfmAPI.js'
 
     export default {
         name: 'rowList',
@@ -29,99 +31,19 @@
                 albumData: []
             }
         },
-        created: function() {
-            this.retrieveAlbumData();
+        created: async function() {
+            this.albumData = await this.retrieveAlbumData();
+            this.albumData = this.albumData.results.albummatches.album;
             console.log(this.albumData);
         },
         methods: {
             async retrieveAlbumData() {
-                this.albumData = await getAlbumData();
+                return await getAlbumData();
+                //console.log(this.albumData);
             }
         }
-        // data() {
-        //     return {
-        //         "albums": [
-        //             {
-        //                 "name": "frailty",
-        //                 "artist": "Jane Remover",
-        //                 "url": "https://www.last.fm/music/Jane+Remover/frailty",
-        //                 "image": [
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/34s/543cc0aee1c6e279f9d8ce4d258bf16d.png",
-        //                         "size": "small"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/64s/543cc0aee1c6e279f9d8ce4d258bf16d.png",
-        //                         "size": "medium"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/174s/543cc0aee1c6e279f9d8ce4d258bf16d.png",
-        //                         "size": "large"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/543cc0aee1c6e279f9d8ce4d258bf16d.png",
-        //                         "size": "extralarge"
-        //                     }
-        //                 ],
-        //                 "streamable": "0",
-        //                 "mbid": ""
-        //             },
-        //             {
-        //                 "name": "teen week",
-        //                 "artist": "Jane Remover",
-        //                 "url": "https://www.last.fm/music/Jane+Remover/teen+week",
-        //                 "image": [
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/34s/0a6de4088726ae91d043d16dc822d6ff.png",
-        //                         "size": "small"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/64s/0a6de4088726ae91d043d16dc822d6ff.png",
-        //                         "size": "medium"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/174s/0a6de4088726ae91d043d16dc822d6ff.png",
-        //                         "size": "large"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/0a6de4088726ae91d043d16dc822d6ff.png",
-        //                         "size": "extralarge"
-        //                     }
-        //                 ],
-        //                 "streamable": "0",
-        //                 "mbid": ""
-        //             },
-        //             {
-        //                 "name": "Royal Blue Walls",
-        //                 "artist": "Jane Remover",
-        //                 "url": "https://www.last.fm/music/Jane+Remover/Royal+Blue+Walls",
-        //                 "image": [
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/34s/18ad96e95badb33b0bf5a0278baaf4ac.png",
-        //                         "size": "small"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/64s/18ad96e95badb33b0bf5a0278baaf4ac.png",
-        //                         "size": "medium"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/174s/18ad96e95badb33b0bf5a0278baaf4ac.png",
-        //                         "size": "large"
-        //                     },
-        //                     {
-        //                         "#text": "https://lastfm.freetls.fastly.net/i/u/300x300/18ad96e95badb33b0bf5a0278baaf4ac.png",
-        //                         "size": "extralarge"
-        //                     }
-        //                 ],
-        //                 "streamable": "0",
-        //                 "mbid": ""
-        //             }
-        //         ]
-        //     }
-        // }
     }
 
-    import {getAlbumData} from '@/services/api/lastfmAPI.js'
     //let data = getAlbumData();
     //data.then(result => {console.log(result);})
 
